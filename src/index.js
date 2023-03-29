2e449da1b08751a7e64ede785acab1e461f7feb9
Íñigo Marquínez Prado <25435858+inigomarquinez@users.noreply.github.com>
2023-03-29 16:44:52 +0200

    fix: wait for validator initialization

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const { logger } = require('./utils')
+const server = require('./server')
 
-const server = require('./server')({
-  swaggerEnabled: true
-})
+async function start() {
+  const app = await server({
+    swaggerEnabled: true
+  })
 
-const port = process.env.PORT || 3000
+  const port = process.env.PORT || 3000
+  app.listen(port, () => logger.info(`Server listening on port ${port}!`))
+}
 
-server.listen(port, () => logger.info(`Server listening on port ${port}!`))
+start()
