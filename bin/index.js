b6f55307042607b0cef268d850e1e09398da6087
Ulises Gascon <ulisesgascongonzalez@gmail.com>
2023-04-27 14:36:53 +0200

    fix: Added async support for CLI initialization

--- bin/index.js
+++ bin/index.js
@@ -1,13 +1,15 @@
 #!/usr/bin/env node
-
 const { logger } = require('../src/utils')
+const appInitialization = require('../src/server')
 
-const app = require('../src/server')({
-  swaggerEnabled: true
-})
+;(async () => {
+  const app = await appInitialization({
+    swaggerEnabled: true
+  })
 
-const port = parseInt([process.argv[2]]) || 0
+  const port = parseInt([process.argv[2]]) || 0
 
-const server = app.listen(port, () =>
-  logger.info(`Server listening on port ${server.address().port}!`)
-)
+  const server = app.listen(port, () =>
+    logger.info(`Server listening on port ${server.address().port}!`)
+  )
+})()
